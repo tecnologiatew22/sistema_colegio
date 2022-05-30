66ae48dde3ff83e9f7daedcdefed3915c2f0a2ef
tecnologiatew22 <106041606+tecnologiatew22@users.noreply.github.com>
2022-05-29 22:08:21 -0400

    Se realizo el enlace para alumno nuevo.php, listar.php

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <title>Sistema Colegio</title>
     <link rel="stylesheet" href="/sistema_colegio/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container">
         <div class="row">
@@ -20,77 +22,77 @@
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-                        </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="./index.php">Inicio <span class="sr-only">(current)</span></a>
+                            </li>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Alumno
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Nuevo Alumno</a>                            
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Listado Alumno</a>
-                            </div>
-                        </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                    Alumno
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="alumno/nuevo.php">Nuevo Alumno</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="alumno/listar.php">Listado Alumno</a>
+                                </div>
+                            </li>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Materia
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Nuevo Materia</a>                            
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Listado Materia</a>
-                            </div>
-                        </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                    Materia
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Nuevo Materia</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Listado Materia</a>
+                                </div>
+                            </li>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Inscripción
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Nuevo Inscripción</a>                            
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Listado Inscripción</a>
-                            </div>
-                        </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                    Inscripción
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Nuevo Inscripción</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Listado Inscripción</a>
+                                </div>
+                            </li>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Reporte
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Nuevo Reporte</a>                            
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Listado Reporte</a>
-                            </div>
-                        </li>                        
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                    Reporte
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Nuevo Reporte</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Listado Reporte</a>
+                                </div>
+                            </li>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Usuario
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Nuevo Usuario</a>                            
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Listado Usuario</a>
-                            </div>
-                        </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                    Usuario
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Nuevo Usuario</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Listado Usuario</a>
+                                </div>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Salir</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Salir</a>
+                            </li>
                     </div>
-                    </nav>
+                </nav>
             </div>
         </div>
 
         <div class="row">
             <div class="col-md-12">
-                <div class="card">         
+                <div class="card">
                     <div class="card-header">
                         <h6><?php echo $titulo; ?></h6>
-                    </div>           
-                    <div class="card-body">                        
+                    </div>
+                    <div class="card-body">
